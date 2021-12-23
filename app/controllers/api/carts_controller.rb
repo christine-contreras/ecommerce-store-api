645185615846856dd242b62b5f57054424dda8a8
Christine Contreras <christineec23@yahoo.com>
2021-12-23 09:09:00 -0600

    update setting cart session

--- app/controllers/api/carts_controller.rb
+++ app/controllers/api/carts_controller.rb
@@ -1,18 +1,17 @@
 class Api::CartsController < ApplicationController
     rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
-    before_action :set_cart
+    # before_action :set_cart
     skip_before_action :authorize
     
 
     def show 
-        if !@cart
-            @cart = Cart.new
-            if @current_user 
-                @cart.user = @current_user
-            end
-            @cart.save
-
-            session[:cart_id] = @cart.id
+        byebug
+        if session.include? :cart_id
+            byebug
+            set_cart
+        else
+            byebug
+            create_cart
         end
 
         render json: @cart
@@ -21,7 +20,18 @@ class Api::CartsController < ApplicationController
     private
 
     def set_cart
-        @cart = Cart.find_by(session[:cart_id])
+        @cart = Cart.find_by(id: session[:cart_id])
+    end
+
+    def create_cart
+        @cart = Cart.new
+        if @current_user 
+            @cart.user = @current_user
+        end
+        @cart.save
+        byebug
+        session[:cart_id] = @cart.id
+        @cart
     end
 
     def render_not_found_response

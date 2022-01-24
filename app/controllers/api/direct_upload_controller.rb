b59220dfd897def660e5c9ebff191fc8ea647f93
Christine Contreras <christineec23@yahoo.com>
2022-01-24 17:07:37 -0600

    update only sending filename to api for aws

--- app/controllers/api/direct_upload_controller.rb
+++ app/controllers/api/direct_upload_controller.rb
@@ -2,24 +2,17 @@ class Api::DirectUploadController < ApplicationController
     # skip_before_action :authorize
 
     def create 
-        image = params[:image]
+        image = params[:image_name]
         bucket = S3_BUCKET
-        object = bucket.object(image.original_filename)
+        object = bucket.object(image)
 
         if object.exists? 
             url = nil
         else #object already exists in bucket
-            url = bucket.object(image.original_filename).presigned_url(:put)
+            url = bucket.object(image).presigned_url(:put)
         end
-        
+
         render json: { url: url}
-    end
-
-
-    private 
-
-    def blob_params
-        params.permit(:image)
     end
 
 end
